a40ee5822a1ba4911056ec7327c5548f6ea2aa90
Oisín Mac Fhearaí <denpashogai@gmail.com>
2021-12-02 05:38:30 +0000

    Writeup / refactor

--- day2/Part2.hs
+++ day2/Part2.hs
@@ -2,27 +2,30 @@ module Main where
 
 import Text.Parsec
 
+data Dir = Forward | Down | Up deriving (Show,Eq)
+
 main = do
   txt <- readFile "input.txt"
-  --putStrLn txt
   let (Right dirs) = parse parser "" txt
-  --print dirs
-  let pos = calcPos dirs
-  print pos
+  print $ calcPos dirs
 
 calcPos dirs = h*d
   where (h,d,a) = foldl (\(h,d,a) (dir,n) -> case dir of
-                        "forward" -> (h+n,d+n*a,a)
-                        "down" -> (h,d,a+n)
-                        "up" -> (h,d,a-n)) (0,0,0) dirs
+                        Forward -> (h+n,d+n*a,a)
+                        Down -> (h,d,a+n)
+                        Up -> (h,d,a-n)) (0,0,0) dirs
 
-parser :: Parsec String () [(String,Int)]
+parser :: Parsec String () [(Dir,Int)]
 parser = do
   (do
-    dir <- many1 letter
+    dir <- getDir <$> many1 letter
     space
     n <- number
     return (dir,n)) `sepEndBy` newline
 
 number = read <$> many1 digit
---number = many1 digit
+
+getDir "forward" = Forward
+getDir "down" = Down
+getDir "up" = Up
+getDir s = error $ "Invalid direction specified: " ++ s
